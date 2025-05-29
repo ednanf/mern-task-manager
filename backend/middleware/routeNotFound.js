24a3b5a8ac57420897fce7f6268a06afc33602d5
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-05-29 13:52:07 -0300

    Refactor routeNotFound middleware to import HttpError using destructuring

--- backend/middleware/routeNotFound.js
+++ backend/middleware/routeNotFound.js
@@ -1,5 +1,5 @@
 // This is needed because Express does not treat "no route matched" as an error!
-const HttpError = require('../errors/HttpError');
+const { HttpError } = require('../errors');
 
 const routeNotFound = (req, res, next) => {
   const error = new HttpError(404, 'Route not found');
