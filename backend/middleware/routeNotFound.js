c577c04dc46da5d853bb68e45c0321a1520be5e8
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-05-29 13:00:19 -0300

    Add comment to clarify purpose of routeNotFound middleware

--- backend/middleware/routeNotFound.js
+++ backend/middleware/routeNotFound.js
@@ -1,3 +1,5 @@
+// This is needed because Express does not treat "no route matched" as an error!
+
 const routeNotFound = (req, res, next) => {
   const error = new Error('404 - Not found.');
   error.status = 404;
