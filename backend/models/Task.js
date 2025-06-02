04f3ca1f7caa91ddc9aeb4491388f8def9cee228
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-06-02 14:25:37 -0300

    Add createdBy property to Task schema for user assignment

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,30 +1,24 @@
 const mongoose = require('mongoose');
 
-/*
-TODO: Add a createdBy property to assing the tasks to a specific user.
-It should be something like this: 
-
-createdBy: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User'
-    required: [true, 'Provide an user.']
-},
-
-This way, the task will be tied to a specific user.
-NOTE: 'User' should be replaced by whatever name the user model has.
-*/
-
-const TaskSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    trim: true,
-    required: [true, 'Task title is required.'],
-    maxLength: [20, 'Task title must contain a maximum of 20 characters.'],
+const TaskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: [true, 'Task title is required.'],
+      maxLength: [20, 'Task title must contain a maximum of 20 characters.'],
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Provide an user.'],
+    },
   },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-}, {timestamps: true});
+  { timestamps: true },
+);
 
 module.exports = mongoose.model('Task', TaskSchema);
