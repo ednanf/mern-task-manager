17df122fea88eceb17f6b07083855e01e6e30c2b
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-06-02 12:01:06 -0300

    Fix jwt util

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 const createToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: JWT_LIFETIME });
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
 };
 
 module.exports = createToken;
