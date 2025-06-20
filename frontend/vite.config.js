57bb6bbe4e0266fb32cf0988848d01532ca36d56
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-06-20 14:55:43 -0300

    Refactor Vite config: streamline server configuration for production environment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,18 @@ import fs from 'fs';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 
+// Only use HTTPS config in local development
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-    },
-    host: 'localhost', // Optional
-    port: 5173, // Optional
-  },
+  server:
+    process.env.VITE_ENV === 'PROD' || process.env.NODE_ENV === 'production'
+      ? {}
+      : {
+          https: {
+            key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+            cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+          },
+          host: 'localhost', // Optional
+          port: 5173, // Optional
+        },
   plugins: [react()],
 });
